e27dd57ae285a87252af64135c1bac50e4a00e4b
bikang1 <bikang1@staff.weibo.com>
2021-07-03 11:09:29 +0800

    add juan

--- juan.go
+++ juan.go
@@ -2,11 +2,17 @@ package main
 
 import "fmt"
 
+var times float32 = 0.1
+
+const BASE = 525600
+
 func main() {
-	fmt.Println("beautiful world")
+	fmt.Println("beautiful world.  start juan")
 	juan()
 }
 
 func juan() {
+	times += 1
+	fmt.Printf("year:%f \n", times/BASE)
 	juan()
 }
